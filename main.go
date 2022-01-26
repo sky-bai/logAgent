3991dd9e68841b0b59fa876913bfc1720ccf8fe7
sky-bai <569531262@qq.com>
2022-01-26 21:28:56 +0800

    es etcd

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/sirupsen/logrus"
 	"logAgent/common"
 	"logAgent/es"
+	"logAgent/etcd"
+	"logAgent/kafka"
+	"logAgent/tailfile"
 	"net"
 )
 
@@ -42,40 +45,40 @@ func main() {
 
 	fmt.Println("加载配置文件成功")
 
-	//// 2. 初始化kafka
-	//err = kafka.Init([]string{configObj.KafkaConfig.Address}, configObj.KafkaConfig.ChanSize)
-	//if err != nil {
-	//	logrus.Error("kafka init error:%v", err)
-	//	return
-	//}
-	//logrus.Info("kafka 初始化成功")
-	//
-	//// 3. 初始化etcd 连接
-	//err = etcd.Init([]string{configObj.EtcdConfig.Address})
-	//if err != nil {
-	//	logrus.Errorf("etcd init error:%v", err)
-	//	return
-	//}
-	//logrus.Info("etcd 初始化成功")
-	////collectKey := fmt.Sprintf(configObj.EtcdConfig.CollectKey, ip)
-	//
-	//// 4. 从etcd中获取要搜集日志的所有配置项 json格式的字符串
-	//allConf, err := etcd.GetConf(configObj.EtcdConfig.CollectKey)
-	//if err != nil {
-	//	logrus.Errorf("etcd get conf error:%v", err)
-	//	return
-	//}
-	//fmt.Println("初始化获取到的配置:", allConf)
-	//
-	//// 5.监听etcd中的配置项变化
-	//go etcd.WatchConf(configObj.EtcdConfig.CollectKey)
-	//
-	//// 5. 根据全部配置中的日志路径初始化tail tail只能获取一个日志文件地址然后创建一个tail对象
-	//err = tailfile.Init(allConf)
-	//if err != nil {
-	//	logrus.Error("tailfile init error:%v", err)
-	//}
-	//logrus.Info("日志文件tailfile 初始化成功")
+	// 2. 初始化kafka
+	err = kafka.Init([]string{configObj.KafkaConfig.Address}, configObj.KafkaConfig.ChanSize)
+	if err != nil {
+		logrus.Error("kafka init error:%v", err)
+		return
+	}
+	logrus.Info("kafka 初始化成功")
+
+	// 3. 初始化etcd 连接
+	err = etcd.Init([]string{configObj.EtcdConfig.Address})
+	if err != nil {
+		logrus.Errorf("etcd init error:%v", err)
+		return
+	}
+	logrus.Info("etcd 初始化成功")
+	//collectKey := fmt.Sprintf(configObj.EtcdConfig.CollectKey, ip)
+
+	// 4. 从etcd中获取要搜集日志的所有配置项 json格式的字符串
+	allConf, err := etcd.GetConf(configObj.EtcdConfig.CollectKey)
+	if err != nil {
+		logrus.Errorf("etcd get conf error:%v", err)
+		return
+	}
+	fmt.Println("初始化获取到的配置:", allConf)
+
+	// 5.监听etcd中的配置项变化
+	go etcd.WatchConf(configObj.EtcdConfig.CollectKey)
+
+	// 5. 根据全部配置中的日志路径初始化tail tail只能获取一个日志文件地址然后创建一个tail对象
+	err = tailfile.Init(allConf)
+	if err != nil {
+		logrus.Error("tailfile init error:%v", err)
+	}
+	logrus.Info("日志文件tailfile 初始化成功")
 	fmt.Println(configObj.ESConf.Address)
 	// 6.连接ES
 	err = es.Init(configObj.ESConf.Address, configObj.ESConf.Index, configObj.ESConf.GoroutineNum, configObj.ESConf.MaxSize)
